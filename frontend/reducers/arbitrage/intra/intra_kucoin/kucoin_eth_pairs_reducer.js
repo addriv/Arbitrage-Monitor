10bbaad2095b922d8277b2ff52beb84ef2e8a2bb
addriv <adrivero89@gmail.com>
2018-01-28 23:43:31 -0800

    Add ETH pairs reducer;

---
+++ frontend/reducers/arbitrage/intra/intra_kucoin/kucoin_eth_pairs_reducer.js
@@ -0,0 +1,21 @@
+import {
+  RECEIVE_KUCOIN_NEO_RATIO
+} from "../../../../actions/intra/intra_kucoin_actions";
+
+const _defaultState = {
+  'DBC-NEO': null
+};
+
+const kucoinNEOPairsReducer = (state = _defaultState, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_KUCOIN_NEO_RATIO:
+      const newState = Object.assign({}, state);
+      newState[action.symbol] = action.ratio;
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default kucoinNEOPairsReducer;
--- frontend/reducers/arbitrage/intra/intra_kucoin/kucoin_ratios_reducer.js
+++ frontend/reducers/arbitrage/intra/intra_kucoin/kucoin_ratios_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import kucoinBTCPairsReducer from './kucoin_btc_pairs_reducer';
 import kucoinNEOPairsReducer from './kucoin_neo_pairs_reducer';
+import kucoinETHPairsReducer from './kucoin_eth_pairs_reducer';
 
 const kucoinRatiosReducer = combineReducers({
   BTCPairs: kucoinBTCPairsReducer,
-  NEOPairs: kucoinNEOPairsReducer
+  NEOPairs: kucoinNEOPairsReducer,
+  ETHPairs: kucoinETHPairsReducer
 });
 
 export default kucoinRatiosReducer;
